e89249272ad270221540fbe51b7bc8a58ed41bdf
Swarup Hegde <swaruphegde@gmail.com>
2020-10-04 12:13:55 +0530

    rem: email checks

--- login/server/validation/register.js
+++ login/server/validation/register.js
@@ -13,13 +13,8 @@ module.exports = function validateRegisterInput(data) {
     if(Validator.isEmpty(data.name)) {
         errors.name = "Name field is required"
     }
-
-    // email checks
-    if(Validator.isEmpty(data.email)){
-        errors.email = "Email field is required"
-    } else if( !Validator.isEmail(data.email)){
-        errors.email = "Email is invalid"
-    }
++
+    
 
     // password checks 
     if(Validator.isEmpty(data.password)) {
